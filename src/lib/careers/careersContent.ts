1fe8c671bf425331608c8ba4b22b45260eee43f9
Julien Newman <newmanjulien@users.noreply.github.com>
2026-06-05 10:15:30 -0400

    careers

--- src/lib/careers/careersContent.ts
+++ src/lib/careers/careersContent.ts
@@ -11,22 +11,21 @@ export type CareersPageContent = {
 };
 
 export const careersPageContent = {
-  title: 'Careers at Overbase',
+  title: "Careers at Overbase",
   paragraphs: [
-    'We only hire engineers. We hire 99.9th percentile most smart people. And we never hire anyone who has experience',
+    "We only hire engineers. We hire 99.9th percentile most smart people. And we never hire anyone who has experience",
     "Reach out if you're an engineering undergrad at MIT or Stanford. Or a CS undergrad at IIT Bombay/Delhi with an AIR of 1 - 50 in the JEE",
-    "We also don't care about your visa status"
   ],
   links: [
     {
-      label: 'Product Engineer 1 - USA',
-      href: 'https://drive.google.com/file/d/1rCa5Q-wEGkV5wYfdNe4tH1Yn8pFnr3WE/view'
+      label: "Product Engineer 1 - USA",
+      href: "https://drive.google.com/file/d/1rCa5Q-wEGkV5wYfdNe4tH1Yn8pFnr3WE/view",
     },
     {
-      label: 'Product Engineer 1 - India',
-      href: 'https://drive.google.com/file/d/11chLFScixDu3RtAG5jKFw6UphwMp4FMF/view'
-    }
+      label: "Product Engineer 1 - India",
+      href: "https://drive.google.com/file/d/11chLFScixDu3RtAG5jKFw6UphwMp4FMF/view",
+    },
   ],
   closingParagraph:
-    "Overbase is a small team and we don't have a location. We relocate as a group to wherever customers need us most and work from there"
+    "Overbase is a forward deployed company. We relocate as a group to wherever customers need us most and work together from there",
 } as const satisfies CareersPageContent;
