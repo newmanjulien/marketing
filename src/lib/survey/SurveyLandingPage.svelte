ef1ac12f06a0fc368e915ba040905006870a98a8
Julien Newman <newmanjulien@users.noreply.github.com>
2026-07-01 13:12:00 -0400

    tweaked survey text

--- src/lib/survey/SurveyLandingPage.svelte
+++ src/lib/survey/SurveyLandingPage.svelte
@@ -74,7 +74,7 @@
 
       <p class="mt-[20px] text-[16px] font-normal leading-[1.55] tracking-normal text-stone-700">
         Every year, Overbase surveys the marketing teams in the best professional services firms to
-        find out who are the most innovative CMOs in each industry according to their peers.
+        find out who you believe are the most innovative CMOs in your industry
       </p>
 
       <img
@@ -88,25 +88,15 @@
 
     <SurveyResultsSignup form={form ?? undefined} />
 
-    <p class="mt-[44px] text-[16px] font-normal leading-[1.55] tracking-normal text-stone-700">
-      Every year, Overbase surveys the marketing teams of the best professional services firms to
-      find the most innovative CMOs in each industry.
-    </p>
-
-    <h2
-      class="mt-[50px] font-heading text-[22px] font-medium leading-[1.05] tracking-normal text-stone-900"
-    >
-      Most innovative CMOs in each industry
-    </h2>
     <div
       class="mt-[20px] flex flex-col gap-[20px] text-[16px] font-normal leading-[1.55] tracking-normal text-stone-700"
     >
       <p>
         Overbase surveys marketing teams from the best insurance brokerages, law firms, government
-        relations firms, consulting firms and accounting firms.
+        relations firms, consulting firms and accounting firms
       </p>
 
-      <p>Then we publish a list of the most innovative CMOs in each industry.</p>
+      <p>Then we publish a list of who you think are the most innovative CMOs in your industry</p>
     </div>
 
     <h2
@@ -118,22 +108,17 @@
       class="mt-[20px] flex flex-col gap-[20px] text-[16px] font-normal leading-[1.55] tracking-normal text-stone-700"
     >
       <p>
-        We send our survey to each firm's marketing team. Our list is entirely based on the
-        responses to this survey.
+        We send our survey to each firm's marketing team. Our results are entirely based on your
+        responses to this survey
       </p>
 
-      <p>We only survey marketing teams from the best firms in each industry:</p>
+      <p>We only survey marketing teams from:</p>
 
       <ul class="list-disc pl-[32px]">
         {#each firmCriteria as criterion}
           <li>{criterion}</li>
         {/each}
       </ul>
-
-      <p>
-        Nothing but survey results from peers is taken into account. And there's no way to request
-        a survey, ask for a firm to be included or nominate a CMO.
-      </p>
     </div>
 
     <h2
@@ -143,7 +128,7 @@
     </h2>
     <p class="mt-[20px] text-[16px] font-normal leading-[1.55] tracking-normal text-stone-700">
       +900 marketers from the best professional services firms answered our survey last year.
-      Check out last year's lists for each industry.
+      Check out last year's lists for each industry
     </p>
     <div
       class="mt-[20px] flex flex-col gap-[20px] text-[16px] font-normal leading-[1.55] tracking-normal text-stone-700 [&_a]:text-stone-500 [&_a]:underline [&_a]:decoration-current [&_a]:underline-offset-[3px] [&_a]:transition-colors [&_a:hover]:text-stone-750"
