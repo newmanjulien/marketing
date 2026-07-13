9450ce663b2892fff33b2f9f5e3bd91096e3cb7b
Julien Newman <newmanjulien@users.noreply.github.com>
2026-07-13 12:30:34 -0400

    accounting

--- src/lib/home/features/opportunity-email/OpportunityEmailGraphic.svelte
+++ src/lib/home/features/opportunity-email/OpportunityEmailGraphic.svelte
@@ -45,7 +45,7 @@ Our review of Linamar’s tax return suggests the client is expanding into Europ
 
 This may be an opportunity to help them assess Privacy & Data Protection considerations. Tax Partner Scott Duarte is the right person to connect with for details. 
 
-Topics to explore with Scott: new sources of revenue outside of the US, increased travel activities in Europe, new professional services engagements in Europe.`
+Topics to explore to understand the tax return: new sources of revenue outside of the US, increased travel activities in Europe, new professional services engagements in Europe.`
   } as const satisfies Record<HomeIndustryId, string>;
 
   const opportunityExplanationByIndustryId = {
